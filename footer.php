dbe0372600fc6da84dee8428898c9931a8503b72
cheythi <chethanageeth@gmail.com>
2020-10-14 11:54:00 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,85 @@
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="footer_title">
+					<h4>Musaeus College <br> Colombo</h4>
+				</div>
+				<div class="footer_content">
+					<div class="footer_logo">
+						<img src="assets/badge.png" alt="" class="img-responsive">
+					</div>
+					<p>About school Bla bla bla bla bla bla bla bla bla blaaaaaaaaaa blaaaaa bla blaaaaaaaa blaaaaaaaaa </p>
+					
+					<div class="footer_social">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="footer_title">
+					<h4>Explor link </h4>
+				</div>
+				<div class="footer_content">
+					<ul>
+						<li><a href="#"><span><i class="fa fa-caret-right" aria-hidden="true"></i></span>Home</a></li>
+						<li><a href="#"><span><i class="fa fa-caret-right" aria-hidden="true"></i></span>About</a></li>
+						<li><a href="#"><span><i class="fa fa-caret-right" aria-hidden="true"></i></span>Service</a></li>
+						<li><a href="#"><span><i class="fa fa-caret-right" aria-hidden="true"></i></span>Gallery</a></li>
+						<li><a href="#"><span><i class="fa fa-caret-right" aria-hidden="true"></i></span>Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="footer_title">
+					<h4>Contact us </h4>
+				</div>
+				<div class="footer_content">
+					<ul class="contact_details">
+						<li><a href="#"><span><i class="fa fa-map-marker" aria-hidden="true"></i></span>No 58, Rosmead Place, Colombo 7, Sri Lanka.</a></li>
+						<li><a href="#"><span><i class="fa fa-envelope" aria-hidden="true"></i></span>info@musaeus.lk</a></li>
+						<li><a href="#"><span><i class="fa fa-phone" aria-hidden="true"></i></span>0094-011-2695048<br>0094-011-2686532</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+
+
+
+
+
+
+
+	
+<!-- Bootstrap -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+<!-- Bootstrap End -->
+<script src="js/jquery.easeScroll.js"></script>
+<script src="js/wow.min.js"></script>
+<!-- Owl Js -->
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/custome.js"></script>
+<script>
+	$(document).ready(function() {
+	 $('.gallery').magnificPopup({
+		  delegate: 'a', // child items selector, by clicking on it popup will open
+		  type: 'image'
+		  // other options
+		});
+	});
+</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,299 @@
+<?php include 'header.php'; ?>
+<div class="clearfix"></div>
+
+<section id="banner">
+	<div class="owl-carousel owl-theme">
+	    <div class="item">
+	    	<div class="single_banner" style="background: url(assets/musaeus1.png);">
+	    		<div class="content">
+					<h2 class="wow">Musaeus College<br><span>Colombo 7</span></h2>
+					<!-- hidden -->
+	    			<p class="wow slideInLeft header-cheat">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
+	    			<div class="banner_btn wow slideInRight">
+	    				<a href="#" class="header-cheat">Our Service</a>
+	    				<a href="#" class=header-cheat>Contact Us</a>
+					</div>
+					<!-- end -->
+	    		</div>
+	    	</div>
+	    </div>
+
+	    <div class="item">
+	    	<div class="single_banner" style="background: url(assets/musaeus2.png);">
+	    		<div class="content">
+	    			<h2 class="wow">Musaeus College<br><span>Colombo 7</span></h2>
+	    			<!-- hidden -->
+	    			<p class="wow slideInLeft header-cheat">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
+	    			<div class="banner_btn wow slideInRight">
+	    				<a href="#" class="header-cheat">Our Service</a>
+	    				<a href="#" class=header-cheat>Contact Us</a>
+					</div>
+					<!-- end -->
+	    		</div>
+	    	</div>
+	    </div>
+
+	    <div class="item">
+	    	<div class="single_banner" style="background: url(assets/musaeus3.png);">
+	    		<div class="content">
+	    			<h2 class="wow">Musaeus College<br><span>Colombo 7</span></h2>
+	    			<!-- hidden -->
+	    			<p class="wow slideInLeft header-cheat">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
+	    			<div class="banner_btn wow slideInRight">
+	    				<a href="#" class="header-cheat">Our Service</a>
+	    				<a href="#" class=header-cheat>Contact Us</a>
+					</div>
+					<!-- end -->
+	    		</div>
+	    	</div>
+	    </div>
+	</div>
+</section>
+<div class="clearfix"></div>
+
+<hr>
+<section id="home-section">
+	<div class="container home_btn_row">
+		<div class="row">
+			<div class="col-md-4 col-sm-12">
+				<div class="btn btn-warning home_btn_row_style flex">Guest Book</div>
+			</div>
+			<div class="col-md-4 col-sm-12 resp_head_btn">
+				<div class="btn btn-warning home_btn_row_style flex">Musaeus miscellany</div>
+			</div>
+			<div class="col-md-4 col-sm-12 resp_head_btn">
+				<div class="btn btn-warning home_btn_row_style flex">Media center</div>
+			</div>
+		</div>
+	</div>
+</section>	
+<div class="clearfix">
+
+<hr>
+
+<section id="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+				<div class="about_content">
+					<h2 class="wow slideInDown" data-wow-duration=".8s" data-wow-delay=".5s">Musaeus College</h2>
+					<hr>
+					<p class="wow slideInDown" data-wow-duration=".8s" data-wow-delay="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+					<p class="wow slideInDown" data-wow-duration=".8s" data-wow-delay="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+					<hr>
+					<a href="#" class="btn btn-warning">Read More</a>
+				</div>
+			</div>
+			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+				<div class="about_img wow slideInRight mari" style="margin-left:10rem;">
+					<img src="assets/musaeus15.jpg" alt="" class="img-responsive">
+				</div>
+			</div> 
+		</div>
+	</div>
+</section>
+<div class="clearfix"></div>
+
+<section id="choose">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-ms-4 col-sm-12 col-xs-12">
+				<div class="single_box wow slideInUp" data-wow-duration=".5s" data-wow-delay=".3s">
+					<div class="img_box">
+						<!-- <img src="images/icon1.png" alt="" class="img-responsive"> -->
+						<div class="card">
+						<img class="card-img-top img_menu" src="assets/musaeus12.jpg" alt="Card image cap">
+						<div class="card-body card_btn">
+							<a href="#" class="btn btn-warning" style="width:25rem; text-transform:uppercase;">Outside the classroom</a>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-ms-4 col-sm-12 col-xs-12">
+				<div class="single_box wow slideInUp" data-wow-duration=".5s" data-wow-delay=".3s">
+					<div class="img_box">
+						<!-- <img src="images/icon1.png" alt="" class="img-responsive"> -->
+						<div class="card">
+						<img class="card-img-top img_menu" src="assets/musaeus12.jpg" alt="Card image cap">
+						<div class="card-body card_btn">
+							<a href="#" class="btn btn-warning" style="width:25rem; text-transform:uppercase;">The nursery lower/Upper</a>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-ms-4 col-sm-12 col-xs-12">
+				<div class="single_box wow slideInUp" data-wow-duration=".5s" data-wow-delay=".3s">
+					<div class="img_box">
+						<!-- <img src="images/icon1.png" alt="" class="img-responsive"> -->
+						<div class="card">
+						<img class="card-img-top img_menu" src="assets/musaeus12.jpg" alt="Card image cap">
+						<div class="card-body card_btn">
+							<a href="#" class="btn btn-warning" style="width:25rem;vtext-transform:uppercase;">The Primary</a>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<div class="clearfix"></div>
+
+<hr>
+
+<section id="news&Events">
+<div class="container-fluid news_btn">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row flex">
+				<h2>News & Events</h2>
+			</div>
+			<div class="topic_underline"></div>
+			<div class="row read_more_homepg">
+				<div class="col-lg-3 col-ms-3 col-sm-6 col-xs-12 resp_news">
+					<div class="card">
+						<div class="card-header">
+							Featured
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Special title treatment</h5>
+							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+							<!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-ms-3 col-sm-6 col-xs-12 resp_news">
+					<div class="card">
+						<div class="card-header" style="text-align:center;">
+							Featured
+						</div>
+						<div class="card-body">
+							<h5 class="card-title" style="text-align: justify;">Special title treatment</h5>
+							<p class="card-text" style="text-align: justify;;">With supporting text below as a natural lead-in to additional content.</p>
+							<!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-ms-3 col-sm-6 col-xs-12 resp_news">
+					<div class="card">
+						<div class="card-header" style="text-align:center;">
+							Featured
+						</div>
+						<div class="card-body">
+							<h5 class="card-title" style="text-align: justify;">Special title treatment</h5>
+							<p class="card-text" style="text-align: justify;">With supporting text below as a natural lead-in to additional content.</p>
+							<!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-ms-3 col-sm-6 col-xs-12 resp_news">
+					<div class="card">
+						<div class="card-header" style="text-align:center;">
+							Featured
+						</div>
+						<div class="card-body">
+							<h5 class="card-title" style="text-align: justify;">Special title treatment</h5>
+							<p class="card-text" style="text-align: justify;">With supporting text below as a natural lead-in to additional content.</p>
+							<!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row read_more_homepg flex">
+			 	<a href="#"><h4>Read more<i class="fa fa-arrow-right" aria-hidden="true"></i></h4></a>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+<div class="clearfix"></div>
+
+<hr>
+
+<section id="service">
+	<div class="container">
+		<div class="row">
+
+			<div class="row flex">
+				<h2>Games & Sports</h2>
+			</div>
+			<div class="topic_underline"></div>
+			<p class="flex">scle games gana monawaahari</p>
+			<br>
+			<div class="service_content">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<a href="#" class="single_service wow slideInLeft" data-wow-duration=".6s" data-wow-delay=".3s">
+						<div class="service_img">
+							<img src="assets/musaeus5.jpg" alt="" class="img-responsive">
+						</div>
+						<div class="service_details under-clr">
+							<h2>Table tennis  </h2>
+							<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<a href="#" class="single_service">
+						<div class="service_img">
+							<img src="assets/musaeus5.jpg" alt="" class="img-responsive">
+						</div>
+						<div class="service_details under-clr">
+							<h2>Hockey  </h2>
+							<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<a href="#" class="single_service wow slideInRight" data-wow-duration=".6s" data-wow-delay=".3s">
+						<div class="service_img">
+							<img src="assets/musaeus5.jpg" alt="" class="img-responsive">
+						</div>
+						<div class="service_details under-clr">
+							<h2>Chess  </h2>
+							<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<a href="#" class="single_service wow slideInLeft" data-wow-duration=".6s" data-wow-delay=".3s">
+						<div class="service_img">
+							<img src="assets/musaeus5.jpg" alt="" class="img-responsive">
+						</div>
+						<div class="service_details under-clr">
+							<h2>Badminton  </h2>
+							<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<a href="#" class="single_service">
+						<div class="service_img">
+							<img src="assets/musaeus5.jpg" alt="" class="img-responsive">
+						</div>
+						<div class="service_details under-clr">
+							<h2>Vollyball  </h2>
+							<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<a href="#" class="single_service wow slideInRight" data-wow-duration=".6s" data-wow-delay=".3s">
+						<div class="service_img">
+							<img src="assets/musaeus5.jpg" alt="" class="img-responsive">
+						</div>
+						<div class="service_details under-clr">
+							<h2>Netball  </h2>
+							<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="row read_more_homepg flex">
+			 	<a href="#"><h4>Read more<i class="fa fa-arrow-right" aria-hidden="true"></i></h4></a>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include 'footer.php'; ?>
